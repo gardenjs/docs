9bcf4cd58ae858edf4598405e2b5151b03313df7
onza <info@martinfarkas.com>
2023-04-17 13:21:50 +0200

    link color only on legal inside div.text

--- src/routes/(subpages)/legalnotice/+page.svelte
+++ src/routes/(subpages)/legalnotice/+page.svelte
@@ -3,18 +3,20 @@
 </script>
 
 <Main>
-  <h1>Impressum</h1>
-  <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
-  <p>Rabbit Development<br>
-  Karlstr. 120<br>
-  76137 Karlsruhe, Germany</p>
-  <p><strong>Vertreten durch:</strong><br>
-  Robin Danzinger, Martin Farkas</p>
-  <h2>Kontakt</h2>
-  <p>Martin Farkas<br>
-  E-Mail: info@websites-graphix.com</p>
-  <h2>EU-Streitschlichtung</h2>
-  <p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br> Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
-  <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
-  <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+  <div class="text">
+    <h1>Impressum</h1>
+    <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+    <p>Rabbit Development<br>
+    Karlstr. 120<br>
+    76137 Karlsruhe, Germany</p>
+    <p><strong>Vertreten durch:</strong><br>
+    Robin Danzinger, Martin Farkas</p>
+    <h2>Kontakt</h2>
+    <p>Martin Farkas<br>
+    E-Mail: info@websites-graphix.com</p>
+    <h2>EU-Streitschlichtung</h2>
+    <p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>. Unsere E-Mail-Adresse finden Sie oben.</p>
+    <h2>Verbraucher&shy;streit&shy;beilegung / Universal&shy;schlichtungs&shy;stelle</h2>
+    <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+  </div>
 </Main>
