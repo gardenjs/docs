5909db4431e31e7f3cc52be586a30c7ae205ab7c
onza <info@martinfarkas.com>
2023-07-04 13:26:37 +0200

    bug

--- src/components/chapterintro/Chapterintro.das.js
+++ src/components/chapterintro/Chapterintro.das.js
@@ -4,7 +4,7 @@ export default {
   description: 'Main chapter intro with h2 title and paragraph.',
   examples: [
     {story: "Intro with superscript, h2 title and intro text", input: {superscript: "Superscript", title: "This is the chapter title", intro: "This is the text. This is the text. This is the text. This is the text. This is the text."}},
-    {story: "Intro with h2 title and intro text", input: {superscript: "Superscript", title: "This is the chapter title", intro: "This is the text. This is the text. This is the text. This is the text. This is the text."}},
+    {story: "Intro with h2 title and intro text", input: {title: "This is the chapter title", intro: "This is the text. This is the text. This is the text. This is the text. This is the text."}},
     {story: "Intro with superscript and h2 title", input: {superscript: "Superscript", title: "This is the chapter title"}}
   ]
 }
