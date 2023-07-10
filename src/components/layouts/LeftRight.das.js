f737d17ff2055e7acd95b3561a6edf5f04107491
onza <info@martinfarkas.com>
2023-07-10 14:40:44 +0200

    moved directly to documentation.svslte

--- src/components/layouts/LeftRight.das.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'LeftRight',
-  file: './LeftRight.svelte',
-  description: 'Layout with 2 columns.',
-  examples: [
-    {story: "Container with sidenav and main container"}
-  ]
-}
