4846bdfcbda63ff4dea25d9035c5084df53e3b27
onza <info@martinfarkas.com>
2023-07-10 14:42:16 +0200

    delete unused code

--- src/components/layouts/Homepage.svelte
+++ src/components/layouts/Homepage.svelte
@@ -9,21 +9,3 @@
 </Header>
 <slot></slot>
 <Footer />
-
-<style lang="scss">
-.cols-3 {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-row-gap: 1rem;
-  margin-bottom: 1rem;
-  @media (min-width: 768px) {
-    grid-template-columns: repeat(2, 1fr);
-    grid-column-gap: 2rem;
-    grid-row-gap: 2rem;
-    margin-bottom: 2rem;
-  }
-  @media (min-width: 1024px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-}
-</style>
