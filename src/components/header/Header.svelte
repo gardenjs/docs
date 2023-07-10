e74c80ba818f6effa00e4d7041c648d421890a8a
onza <info@martinfarkas.com>
2023-07-10 14:40:01 +0200

    add docu to nav, remove nav-toggle

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -3,7 +3,6 @@
   
   export let showStripes = false
   export let showSkew = false
-  export let showSidenav = false
   let currentTheme = 'light'
 
   function toggleTheme() {
@@ -35,23 +34,27 @@
         <img src="src/assets/icons/logo.svg" alt="Logo">
         <span class="logo_label">Garden</span>
       </a>
-      <div class="navbar-btns">
-        <button id="toggle-mode" class="toggle-mode" title="Toggle Website Theme" on:click={toggleTheme}>
-          {#if currentTheme === 'light'}
-          <svg class="icon" role="img" width="32" viewBox="0 0 32 32" height="32" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M16 0c8.836 0 16 7.163 16 16 0 8.836-7.164 16-16 16-8.837 0-16-7.164-16-16C0 7.163 7.162 0 16 0zm-.001 7c-4.937 0-9 4.062-9 9 0 4.936 4.063 8.999 9 8.999s9-4.063 9-9a6.365 6.365 0 01-4.484 1.848c-3.492 0-6.364-2.872-6.364-6.364 0-1.678.664-3.292 1.848-4.483z" fill="#2d466c" fill-rule="evenodd"/></svg>
-          {:else}
-          <svg class="icon" role="img" width="32" viewBox="0 0 32 32" height="32" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M16 0c8.837 0 16 7.163 16 16s-7.163 16-16 16S0 24.837 0 16 7.163 0 16 0zm0 22.2a1 1 0 00-1 1V25a1 1 0 002 0v-1.8a1 1 0 00-1-1zm5.801-1.813a1 1 0 00-1.414 1.414l1.269 1.27a1 1 0 001.414-1.415zm-10.188 0a1 1 0 00-1.414 0l-1.27 1.269a1 1 0 001.415 1.414l1.27-1.269a1 1 0 000-1.414zM16 12.4a3.6 3.6 0 100 7.2 3.6 3.6 0 000-7.2zM8.8 15H7a1 1 0 000 2h1.8a1 1 0 000-2zM25 15h-1.8a1 1 0 000 2H25a1 1 0 000-2zm-1.93-6.07a1 1 0 00-1.414 0l-1.27 1.269a1 1 0 001.415 1.414l1.27-1.269a1 1 0 000-1.414zm-12.726 0a1 1 0 00-1.414 1.414l1.269 1.27a1 1 0 001.414-1.415zM16 6a1 1 0 00-1 1v1.8a1 1 0 002 0V7a1 1 0 00-1-1z" fill="#a5bad9" fill-rule="evenodd"/></svg>
-          {/if}
-        </button>
-        <a class="github" href="https://github.com/rabbitdevelopment/garden" title="Garden Github Repository" target="_blank">
-          <svg class="icon" role="img" width="32" viewBox="0 0 32 32" height="32" xmlns="http://www.w3.org/2000/svg" fill="currentColor" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2"><path d="M15.015 0C6.723 0 0 6.894 0 15.398c0 6.803 4.302 12.573 10.27 14.61.75.14 1.025-.334 1.025-.743 0-.365-.013-1.334-.02-2.618-4.178.93-5.059-2.065-5.059-2.065-.683-1.778-1.668-2.252-1.668-2.252-1.362-.955.104-.936.104-.936 1.507.11 2.3 1.587 2.3 1.587 1.34 2.353 3.515 1.673 4.37 1.28.137-.996.525-1.674.954-2.058-3.334-.389-6.84-1.71-6.84-7.61 0-1.681.585-3.056 1.545-4.132-.155-.39-.67-1.955.147-4.075 0 0 1.26-.414 4.13 1.579a14.03 14.03 0 013.76-.518c1.275.005 2.56.176 3.759.518 2.867-1.993 4.126-1.579 4.126-1.579.82 2.12.304 3.685.15 4.075.961 1.076 1.543 2.45 1.543 4.13 0 5.917-3.512 7.218-6.857 7.599.54.476 1.02 1.415 1.02 2.852 0 2.057-.02 3.718-.02 4.223 0 .413.27.891 1.034.741 5.963-2.04 10.261-7.808 10.261-14.608C30.034 6.894 23.31 0 15.015 0"/></svg>
-        </a>
-        {#if showSidenav}
-          <button id="toggle-nav" class="toggle-nav" aria-haspopup="menu" title="Navigation">
-            <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 12h16M4 6h16M4 18h16"/></svg>
-          </button>
-        {/if}
-      </div>
+      <nav class="navbar_nav">
+        <ul>
+          <li>
+            <a href="/docs" title="go to docs">Docs</a>
+          </li>
+          <li>
+            <button id="toggle-mode" class="toggle-mode" title="Toggle Website Theme" on:click={toggleTheme}>
+            {#if currentTheme === 'light'}
+            <svg class="icon" role="img" width="32" viewBox="0 0 32 32" height="32" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M16 0c8.836 0 16 7.163 16 16 0 8.836-7.164 16-16 16-8.837 0-16-7.164-16-16C0 7.163 7.162 0 16 0zm-.001 7c-4.937 0-9 4.062-9 9 0 4.936 4.063 8.999 9 8.999s9-4.063 9-9a6.365 6.365 0 01-4.484 1.848c-3.492 0-6.364-2.872-6.364-6.364 0-1.678.664-3.292 1.848-4.483z" fill="#2d466c" fill-rule="evenodd"/></svg>
+            {:else}
+            <svg class="icon" role="img" width="32" viewBox="0 0 32 32" height="32" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M16 0c8.837 0 16 7.163 16 16s-7.163 16-16 16S0 24.837 0 16 7.163 0 16 0zm0 22.2a1 1 0 00-1 1V25a1 1 0 002 0v-1.8a1 1 0 00-1-1zm5.801-1.813a1 1 0 00-1.414 1.414l1.269 1.27a1 1 0 001.414-1.415zm-10.188 0a1 1 0 00-1.414 0l-1.27 1.269a1 1 0 001.415 1.414l1.27-1.269a1 1 0 000-1.414zM16 12.4a3.6 3.6 0 100 7.2 3.6 3.6 0 000-7.2zM8.8 15H7a1 1 0 000 2h1.8a1 1 0 000-2zM25 15h-1.8a1 1 0 000 2H25a1 1 0 000-2zm-1.93-6.07a1 1 0 00-1.414 0l-1.27 1.269a1 1 0 001.415 1.414l1.27-1.269a1 1 0 000-1.414zm-12.726 0a1 1 0 00-1.414 1.414l1.269 1.27a1 1 0 001.414-1.415zM16 6a1 1 0 00-1 1v1.8a1 1 0 002 0V7a1 1 0 00-1-1z" fill="#a5bad9" fill-rule="evenodd"/></svg>
+            {/if}
+            </button>
+          </li>
+          <li>
+            <a class="github" href="https://github.com/rabbitdevelopment/garden" title="Garden Github Repository" target="_blank">
+              <svg class="icon" role="img" width="32" viewBox="0 0 32 32" height="32" xmlns="http://www.w3.org/2000/svg" fill="currentColor" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2"><path d="M15.015 0C6.723 0 0 6.894 0 15.398c0 6.803 4.302 12.573 10.27 14.61.75.14 1.025-.334 1.025-.743 0-.365-.013-1.334-.02-2.618-4.178.93-5.059-2.065-5.059-2.065-.683-1.778-1.668-2.252-1.668-2.252-1.362-.955.104-.936.104-.936 1.507.11 2.3 1.587 2.3 1.587 1.34 2.353 3.515 1.673 4.37 1.28.137-.996.525-1.674.954-2.058-3.334-.389-6.84-1.71-6.84-7.61 0-1.681.585-3.056 1.545-4.132-.155-.39-.67-1.955.147-4.075 0 0 1.26-.414 4.13 1.579a14.03 14.03 0 013.76-.518c1.275.005 2.56.176 3.759.518 2.867-1.993 4.126-1.579 4.126-1.579.82 2.12.304 3.685.15 4.075.961 1.076 1.543 2.45 1.543 4.13 0 5.917-3.512 7.218-6.857 7.599.54.476 1.02 1.415 1.02 2.852 0 2.057-.02 3.718-.02 4.223 0 .413.27.891 1.034.741 5.963-2.04 10.261-7.808 10.261-14.608C30.034 6.894 23.31 0 15.015 0"/></svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
     <slot></slot>
   </div>
@@ -75,17 +78,21 @@
     max-height: var(--w-website);
   }
   .skew {
-    position: absolute;
-    content: '';
-    width: 100%;
-    height: 300px;
-    bottom: -300px;
-    left: 0;
-    right: 0;
-    transform: skewY(-3deg);
-    transform-origin: top left;
-    background-color: var(--c-website-bg);
-    z-index: 4;
+    display: none;
+    @media (min-width: 769px) {
+      display: block;
+      position: absolute;
+      content: '';
+      width: 100%;
+      height: 300px;
+      bottom: -300px;
+      left: 0;
+      right: 0;
+      transform: skewY(-3deg);
+      transform-origin: top left;
+      background-color: var(--c-website-bg);
+      z-index: 4;
+    }
   }
   .header_container {
     position: relative;
@@ -95,34 +102,41 @@
     z-index: 3;
     .navbar {
       display: flex;
-      justify-content: space-between;
+      flex-direction: column;
       align-items: center;
+      justify-content: center;
       margin: 0.75rem 0;
+      @media (min-width: 450px) {
+        flex-direction: row;
+        justify-content: space-between;
+      }
       @media (min-width: 768px) {
         margin: 1rem 0;
       }
       @media (min-width: 1024px) {
         margin: 2rem 0;
       }
-      height: 60px;
       a.logo {
         position: relative;
         display: block;
         color: var(--c-primary);
         text-decoration: none;
+        @media (max-width: 449px) {
+          width: 166px;
+        }
         img {
           display: inline;
-          width: 60%;
-          height: auto;
+          width: 65px;
+          height: 36px;
           @media (min-width: 768px) {
             width: 80%;
+            height: auto;
           }
           @media (min-width: 1024px) {
             width: 100%;
           }
         }  
         .logo_label {
-          display: inline-block;
           position: absolute;
           top: 50%;
           padding: 0 0 0 0.75rem;
@@ -144,42 +158,43 @@
           transition: color 0.2s;
         }
       }
-      .navbar-btns {
-        display: flex;
-        align-items: center;
-        button.toggle-mode,
-        a.github,
-        button.toggle-nav {
-          .icon {
-            font-size: 1rem;
-            color: var(--c-navbar-icon);
-            @media (min-width: 768px) {
-              font-size: 1.125rem;
+      .navbar_nav {
+        ul {
+          display: inline-flex;
+          align-items: center;
+          justify-content: flex-end;
+          @media (max-width: 449px) {
+            padding: 1.375rem 0 0;
+          }
+          li {
+            margin-left: 1.5rem;
+            &:first-of-type {
+              margin: 0;
             }
-            @media (min-width: 1024px) {
-              font-size: 1.25rem;
+            a {
+              font-weight: 600;
+              border-bottom: 2px solid var(--c-primary);
+              &.github {
+                border: none
+              }
             }
-          }
-        }
-        button.toggle-mode {
-          margin-right: 1rem;
-          cursor: pointer;
-          background-color: transparent;
-          @media (min-width: 1024px) {
-            margin-right: 1.25rem;
-          }
-        }
-        @media (max-width: 767px) {
-          button.toggle-nav {
-            display: block;
-            margin-left: 1rem;
-            background-color: transparent;
-            cursor: pointer;
-          }
-        }
-        @media (min-width: 768px) {
-          .toggle-nav {
-            display: none;
+            button.toggle-mode,
+            a.github {
+              .icon {
+                font-size: 1rem;
+                color: var(--c-navbar-icon);
+                @media (min-width: 768px) {
+                  font-size: 1.125rem;
+                }
+                @media (min-width: 1024px) {
+                  font-size: 1.25rem;
+                }
+              }
+            }
+            button.toggle-mode {
+              cursor: pointer;
+              background-color: transparent;
+            }
           }
         }
       }
