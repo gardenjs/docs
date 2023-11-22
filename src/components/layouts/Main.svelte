aa85d19d7a9809256566fc2d93cdb2def5f19616
onza <info@martinfarkas.com>
2023-11-22 09:22:55 +0100

    add class hasHeaderintro, change sizesto fit with new stripes

--- src/components/layouts/Main.svelte
+++ src/components/layouts/Main.svelte
@@ -2,10 +2,14 @@
   import Stripes from '../stripes/Stripes.svelte'
   import Vegetables from '../vegetables/Vegetables.svelte'
 
+  export let hasHeaderintro = false
+  let intro = 'has-intro'
+  let margin = ''
+
   export let showStripes = false
 </script>
 
-<main class="main">
+<main class="main {hasHeaderintro? intro : margin}">
   <div class="main_container">
     <slot></slot>
   </div>
@@ -15,18 +19,43 @@
   {/if}
 </main>
 
-<style>
+<style lang="scss">
+@use "sass:math";
 :global(.main) {
   position: relative;
   overflow: hidden;
-  margin: 0;
   padding: 0 1rem;
+  @media (min-width: 840px) {
+    padding: 0 1rem;
+  }
+  @media (min-width: 960px) {
+    padding: 0;
+  }
+  &.has-intro {
+    padding-top: 2rem;
+    @media (min-width: 960px) {
+      padding-top: 4rem;
+    }
+    @media (min-width: 1280px) {
+      padding-top: 6rem;
+    }
+  }
 }
 :global(.main_container) {
   position: relative;
-  width: 100%;
   margin: 3rem auto 0;
-  max-width: var(--w-website);
   z-index: 3;
+  @media (min-width: 840px) {
+    margin-right: calc((100%) / 8);
+    margin-left: calc((100%) / 8);
+  }
+  @media (min-width: 960px) {
+    margin-right: calc((100% - 2rem) / 7);
+    margin-left: calc((100% - 2rem) / 7);
+  }
+  @media (min-width: 1312px) {
+    margin: auto;
+    width: 960px;
+  }
 }
 </style>
