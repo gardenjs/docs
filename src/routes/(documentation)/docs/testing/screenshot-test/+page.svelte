7213f90175aa4f25af7324578a34217707e5d3aa
onza <info@martinfarkas.com>
2024-01-17 17:30:10 +0100

    content: meta description

--- src/routes/(documentation)/docs/testing/screenshot-test/+page.svelte
+++ src/routes/(documentation)/docs/testing/screenshot-test/+page.svelte
@@ -4,7 +4,7 @@
 
 <svelte:head>
 	<title>Screenshot test :: Gardenjs Docs</title>
-  <meta name="description" content="This is the documentation of Gardenjs, the fast storybook alternative for React, Vue and Svelte projects." />
+  <meta name="description" content="Screenshot test. This is the documentation of Gardenjs, the fast storybook alternative for React, Vue and Svelte projects." />
 </svelte:head>
 
 <div class="md_file">
