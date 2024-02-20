775f16252a5d73b044ff459ed4ddf08b023417e8
onza <info@martinfarkas.com>
2024-02-20 13:51:47 +0100

    add express server

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const app = express()
+const port = 8080
+
+app.use(express.static('.svelte-kit/output/prerendered'))
+app.use(express.static('.svelte-kit/output/client'))
+
+app.get('/', (req, res) => {
+  res.sendFile('.svelte-kit/output/prerendered/pages/index.html', {root: __dirname})
+})
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`)
+})
