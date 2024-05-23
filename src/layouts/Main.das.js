fe561c4476f574b6d3c3918f20b6becec5b3e2e2
onza <info@martinfarkas.com>
2024-05-23 08:44:46 +0200

    add description

--- src/layouts/Main.das.js
+++ src/layouts/Main.das.js
@@ -1,9 +1,9 @@
 export default {
   name: 'Main',
   file: './Main.example.svelte',
-  description: '<p>Main content area for “Homepage” and “Supage” layouts.</p><p><b>Note:</b>The background is transparent and only gray here for better presentation.</p>',
+  description: '<p>Main content area for “Homepage” and “Subpage” layouts.</p><p><b>Note:</b>The background is transparent and only gray here for better presentation.</p>',
 
   examples: [
-    {title: "Main content area"}
+    {title: 'Main content area'}
   ]
 }
--- src/layouts/Subpage.das.js
+++ src/layouts/Subpage.das.js
@@ -1,7 +1,7 @@
 export default {
   name: 'Subpage',
   file: './Subpage.example.svelte',
-  description: '',
+  description: '<p>Page layout for "Legal Notice" and "Privacy Policy".</p><p><b>Note:</b>The background is transparent and only gray here for better presentation.</p>',
   examples: [
     // {title: "Layout for Subpages"}
   ]
