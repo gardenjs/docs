421e48a7f7952a200dc768c8d73a57fe39984b34
onza <info@martinfarkas.com>
2024-06-13 11:34:47 +0200

    Page translation

--- src/routes/(subpages)/legalnotice/+page.svelte
+++ src/routes/(subpages)/legalnotice/+page.svelte
@@ -8,28 +8,30 @@
 
 <Main>
   <div>
-    <h1>Impressum</h1>
-    <h2>Angaben gemäß § 5 TMG:</h2>
+    <h1>Imprint</h1>
+    <h2>Information according to § 5 TMG (German Telemedia Act)</h2>
     <p>Rabbit Development<br>
     Karlstr. 120<br>
     76137 Karlsruhe, Germany</p>
-    <p><strong>Vertreten durch:</strong><br>
+    <p><strong>Represented by:</strong><br>
     Robin Danzinger, Martin Farkas</p>
-    <h3>Kontakt</h3>
-    <p>E-Mail: <a href="mailto:hello@gardenjs.org">hello@gardenjs.org</a></p>
-    <h3>EU-Streitschlichtung</h3>
-    <p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>. Unsere E-Mail-Adresse finden Sie oben.</p>
-    <h3>Verbraucher&shy;streit&shy;beilegung / Universal&shy;schlichtungs&shy;stelle</h3>
-    <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
-    <h2>Haftungsausschluss (Disclaimer)</h2>
-    <h3>Haftung für Inhalte</h3>
-    <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p>
-    <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
-    <h3>Haftung für Links</h3>
-    <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p>
-    <p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
-    <h3>Urheberrecht</h3>
-    <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
-    <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+    <h3>Contact</h3>
+    <p>E-mail: <a href="mailto:hello@gardenjs.org">hello@gardenjs.org</a></p>
+    <h3>EU dispute resolution</h3>
+    <p>The European Commission provides a platform for online dispute resolution (OS): <a href="https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home.chooseLanguage" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>. You can find our e-mail address in the legal notice above.</p>
+    <h3>Consumer dispute resolution / universal arbitration board</h3>
+    <p>We are not willing or obliged to participate in dispute resolution proceedings before a consumer arbitration board.</p>
+    <h2>Disclaimer</h2>
+    <h3>Liability for content</h3>
+    <p>As a service provider, we are responsible for our own content on these pages in accordance with Section 7 (1) TMG (German Telemedia Act) and general legislation. According to §§ 8 to 10 TMG, however, we as a service provider are not obliged to monitor transmitted or stored third-party information or to investigate circumstances that indicate illegal activity.</p>
+    <p>Obligations to remove or block the use of information in accordance with general legislation remain unaffected by this. However, liability in this respect is only possible from the time of knowledge of a specific infringement. As soon as we become aware of such infringements, we will remove this content immediately.</p>
+    <h3>Liability for links</h3>
+    <p>Our website contains links to external third-party websites over whose content we have no influence. Therefore, we cannot accept any liability for this third-party content. The respective provider or operator of the pages is always responsible for the content of the linked pages. The linked pages were checked for possible legal violations at the time of linking. Illegal content was not recognizable at the time of linking.</p>
+    <p>However, permanent monitoring of the content of the linked pages is not reasonable without concrete evidence of an infringement. If we become aware of any legal infringements, we will remove such links immediately.</p>
+    <h3>Copyright</h3>
+    <p>The content and works created by the site operators on these pages are subject to German copyright law. Duplication, processing, distribution and any kind of exploitation outside the limits of copyright law require the written consent of the respective author or creator. Downloads and copies of this site are only permitted for private, non-commercial use.</p>
+    <p>Insofar as the content on this site was not created by the operator, the copyrights of third parties are respected. In particular, third-party content is identified as such. Should you nevertheless become aware of a copyright infringement, please inform us accordingly. If we become aware of any infringements, we will remove such content immediately.</p>
+    <p>&nbsp;</p>
+    <p>Source: <a href="https://www.e-recht24.de" target="_blank" rel="noreferrer">eRecht24</a>. Translation: <a href="https://www.deepl.com/" target="_blank">DeepL</a>.</p>
   </div>
 </Main>
