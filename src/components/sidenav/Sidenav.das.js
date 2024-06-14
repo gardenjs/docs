a9af6c142e451b183312607490cdc77fd50afcd0
onza <info@martinfarkas.com>
2024-06-14 18:18:33 +0200

    change links to relative

--- src/components/sidenav/Sidenav.das.js
+++ src/components/sidenav/Sidenav.das.js
@@ -1,17 +1,18 @@
 const overview = {title: 'Overview', items: [{ href: "#", label: "Why Gardenjs" }]};
 const getstarted = {title: 'Get Started', items: [
-  { href: "#", label: "Install" },
-  { href: "#", label: "Install Renderer Plugin" },
-  { href: "#settings", label: "Settings" },
-  { href: "example#", label: "Examples" },
-  { href: "#", label: "Hooks" },
-  { href: "#", label: "Decorators" },
+  { href: "./", label: "Install" },
+  { href: "./", label: "Install Renderer Plugin" },
+  { href: "./", label: "Settings" },
+  { href: "./", label: "Examples" },
+  { href: "./", label: "Hooks" },
+  { href: "./", label: "Decorators" },
 ]};
-const renderer = {title: 'Renderer', items: [{ href: "#", label: "Write New Renderer" }]};
-const notes = {title: 'Add Notes', items:[{ href: "#", label: "Add Notes" }]};
-const roadmap = {title: 'What comes next', items:[{ href: "#", label: "Roadmap" }]};
+const renderer = {title: 'Renderer', items: [{ href: "./", label: "Write New Renderer" }]};
+const notes = {title: 'Add Notes', items:[{ href: "./", label: "Add Notes" }]};
+const roadmap = {title: 'What comes next', items:[{ href: "./", label: "Roadmap" }]};
+const github = {title: 'external Link', items:[{ href: "https://github.com/gardenjs/gardenjs", label: "GitHub" }]};
 
-const sidenavFolders = [overview, getstarted, renderer, notes, roadmap];
+const sidenavFolders = [overview, getstarted, renderer, notes, roadmap, github];
 
 export default {
   name: 'Sidenav',
