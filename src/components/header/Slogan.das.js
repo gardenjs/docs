16a440ae828e47d21d47ab05ca59fe85d6020925
onza <info@martinfarkas.com>
2024-06-21 09:11:03 +0200

    correct example name, add example

--- src/components/header/Slogan.das.js
+++ src/components/header/Slogan.das.js
@@ -4,6 +4,7 @@ export default {
   description: 'Is part of the Hero header.',
 
   examples: [
-    {title: 'Internal Link', input: {slogan: 'This. Is. So. <i>Cool!</i>', subtext: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'}},
+    {title: 'Slogan & subtext', input: {slogan: 'This. Is. So. <i>Cool!</i>', subtext: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'}},
+    {title: 'Slogan', input: {slogan: 'This. Is. So. <i>Cool!</i>'}}
   ]
 }
