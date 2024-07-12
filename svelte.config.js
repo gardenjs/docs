44ac0da642f918cfd1154f87757d180667491347
onza <info@martinfarkas.com>
2024-07-12 15:47:51 +0200

    set build folder to docs

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,14 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		appDir: 'app',
-		adapter: adapter(),
+		adapter: adapter(
+			{
+				// default options are shown
+				pages: 'docs',
+				assets: 'docs',
+				fallback: null
+			}
+		),
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH,
 		},
