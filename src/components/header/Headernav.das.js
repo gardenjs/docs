9bf4c3eb22a527141a23a9840fd57aa05f23198b
onza <info@martinfarkas.com>
2024-10-14 08:30:52 +0200

    remove description

--- src/components/header/Headernav.das.js
+++ src/components/header/Headernav.das.js
@@ -1,7 +1,7 @@
 export default {
   name: 'Headernav',
   file: './Headernav.svelte',
-  description: 'Test darkmode/lightmode behavior via the mode icon inside the component.',
+  description: '',
   examples: [
     // {title: 'Headernav'}
   ]
