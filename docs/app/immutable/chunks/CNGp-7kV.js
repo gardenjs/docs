186a3a5bafd00684e8746465fbd7861337f5b191
onza <info@martinfarkas.com>
2025-11-14 16:58:20 +0100

    add new screenshots

--- docs/app/immutable/chunks/CNGp-7kV.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_y1ed4u?.base??"",e=globalThis.__sveltekit_y1ed4u?.assets??s??"";export{e as a,s as b};
---
+++ docs/app/immutable/chunks/JPEDPpTJ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_aj3b4g?.base??"",a=globalThis.__sveltekit_aj3b4g?.assets??s??"";export{a,s as b};
