ad7965e7cb9c0b469931333fb611adffaa53a82b
onza <info@martinfarkas.com>
2025-11-28 17:26:58 +0100

    update docs

---
+++ docs/app/immutable/chunks/CDW77Gav.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_qp1r27?.base??"",a=globalThis.__sveltekit_qp1r27?.assets??s??"";export{a,s as b};
--- docs/app/immutable/chunks/JPEDPpTJ.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_aj3b4g?.base??"",a=globalThis.__sveltekit_aj3b4g?.assets??s??"";export{a,s as b};
