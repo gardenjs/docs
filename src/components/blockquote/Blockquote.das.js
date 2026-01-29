c015540521ae0421660479943b3e0f9fed31c68a
onza <info@martinfarkas.com>
2026-01-29 20:09:15 +0100

    task: add params

--- src/components/blockquote/Blockquote.das.js
+++ src/components/blockquote/Blockquote.das.js
@@ -2,8 +2,25 @@ export default {
   name: 'Blockquote',
   file: './Blockquote.svelte',
   description: '',
-
   examples: [
-    {title: 'Quote & author', input: {quote: '«Building a frontend is like gardening. Grow your components and layouts, maintain them and control their growth. Build great products and reap the rewards of your work!»', author: 'John Doe, coder & gardener'}},
+    {
+      title: 'Quote & author',
+      input: {
+        quote: '«Building a frontend is like gardening. Grow your components and layouts, maintain them and control their growth. Build great products and reap the rewards of your work!»',
+        author: 'John Doe, coder & gardener'
+      }
+    },
+  ],
+  params: [
+    {
+      name: 'quote',
+      type: 'text',
+      label: 'Quote'
+    },
+    {
+      name: 'author',
+      type: 'text',
+      label: 'Author'
+    }
   ]
 }
