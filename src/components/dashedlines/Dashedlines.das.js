aaf33543b352a508375383023511133d18167dca
onza <info@martinfarkas.com>
2026-02-20 19:14:14 +0100

    docs: add boolean parameters for dashed lines component

--- src/components/dashedlines/Dashedlines.das.js
+++ src/components/dashedlines/Dashedlines.das.js
@@ -11,4 +11,13 @@ export default {
     { title: 'Dashed line 6', input: { dashedlines6: true } },
     { title: 'Dashed line 7', input: { dashedlines7: true } },
   ],
+  params: [
+    { name: 'dashedlines1', type: 'boolean', label: 'Dashed line 1' },
+    { name: 'dashedlines2', type: 'boolean', label: 'Dashed line 2' },
+    { name: 'dashedlines3', type: 'boolean', label: 'Dashed line 3' },
+    { name: 'dashedlines4', type: 'boolean', label: 'Dashed line 4' },
+    { name: 'dashedlines5', type: 'boolean', label: 'Dashed line 5' },
+    { name: 'dashedlines6', type: 'boolean', label: 'Dashed line 6' },
+    { name: 'dashedlines7', type: 'boolean', label: 'Dashed line 7' },
+  ],
 }
